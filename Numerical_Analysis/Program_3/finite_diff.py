249228fdcf2564998581890501f6ed002a5ff8b0
A-Merino <A-Merino>
2025-03-29 16:17:46 -0400

    numerical p3 update 1

--- Numerical_Analysis/Program_3/finite_diff.py
+++ Numerical_Analysis/Program_3/finite_diff.py
@@ -1,2 +1,9 @@
 import numpy as np
 import scipy as sc
+
+def finite_diff():
+    '''
+        This function is an implemented version of
+        the finite difference method to solve second
+        order differential equations
+    '''
