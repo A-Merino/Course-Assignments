c1cadc5ba06df55474b1def8acc5af967e1f94a9
A-Merino <alexquiquemerino@gmail.com>
2025-05-03 11:30:06 -0400

    NumAl Prog4 pt1

--- Numerical_Analysis/Program_4/heat.py
+++ Numerical_Analysis/Program_4/heat.py
@@ -1,10 +1,126 @@
-def forward_diff():
-    pass
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def backward_diff():
-    pass
+def sigma(D, k, h):
+    '''
+        This function calculates the value of sigma in the
+        forward difference method for parabolic/heat equations
+
+        Parameters
+        ----------
+            D: int, float
+                The constant infront of u_xx in the PDE
+
+            k: int, float
+                The set change in t we want to estimate
+
+            h: int, float
+                The set change in x we want to estimate
+
+        Returns
+        -------
+            sigma: float
+                The calculated value of sigma
+    '''
+
+    return (D * k) / h**2
+
+
+def triMatrix(diags, n):
+    '''
+        Function that creates the tri-diagonal matrix, which is used in the 
+        numerical methods of finite difference and finite element. This piece
+        of code was taken from Program 3's createA fuction
+
+        Parameters
+        ----------
+            
+            diags: array_like
+                The three diagonal values which are needed for
+                the creation of the matrix
+
+            n: int
+                The size of matrix we would like to output
+
+        Returns
+        -------
+
+            A: 2D array_like
+                The matrix of size n which contains the diagonal values
+                given in the diags parameter
+    '''
+
+    # diags function from numpy
+    off_d = np.diag(diags[0] * np.ones(n - 1), k=1)
+    off_d = off_d + off_d.T
+
+    diag = np.diag(diags[1] * np.ones(n))
+
+    return off_d + diag
+
+
+def exact(t, x):
+
+    return np.exp((2 * t) + x) + np.exp((2 * t) - x)
+
+
+def t0(x):
+    return 2 * np.cosh(x)
+
+def l_bound(t):
+    return 2 * np.exp(2 * t)
+
+
+def r_bound(t):
+    return (np.exp(2)  + 1) * np.exp((2 * t) - 1)
+
+
+
+def forward_diff(D, k, h, ts, xs):
+
+    sig = sigma(D, k, h)
+    matrix = triMatrix([sig, 1 - (2  * sig), sig], len(ts) - 2)
+
+    fac = [sig, 1 - (2  * sig), sig]
+
+
+    u = np.zeros((len(xs), len(ts)))
+
+    u[:,0] = t0(xs)
+
+
+    for j, t in enumerate(ts[1:-1]):
+        u[0, j + 1] = fac[0]* l_bound(t) + fac[1] * u[1,j] + fac[2] * u[2, j]
+
+        u[1:-2, j + 1] = fac[0]* u[0:-3, j] + fac[1] * u[1:-2,j] + fac[2] * u[2:-1, j]
+
+        u[-1, j + 1] = fac[2]* r_bound(t) + fac[1] * u[-1,j] + fac[0] * u[-2, j]
+
+        correct = exact(t, xs)
+
+
+        plt.plot(xs, abs(correct - u[:, j+1]))
+
+    plt.show()
+    print(u)
+    ex = exact(1,xs)
+    est = u[:, -2]
+
+    print(ex)
+    print(est)
+
+    correct = exact(1, xs)
+
+
+
+
+
+
+def backward_diff(D, k, h, ts, xs):
+    sig = sigma(D, k, h)
+    matrix = triMatrix([-sig, 1 + (2 * sig), -sig], len(ts) - 2)
 
 def crank_nicolson():
-
+    pass
 
--- Numerical_Analysis/Program_4/main.py
+++ Numerical_Analysis/Program_4/main.py
@@ -5,6 +5,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
+    k = [0.25, 0.125, 0.0625, 0.03125]
+    h = [0.25, 0.125, 0.0625, 0.03125]
+
+
+    part1a(k, h)
+    # part1b(k, h)
+    # part1c(k, h)
+    # part2()
+    # part3b()
+    # part3c()
+
+
+
+def part1a(k, h):
+    D = 2
+    for t, x in zip(k, h):
+        xs = np.linspace(0, 1, int(1/x) + 1)
+        ts = np.linspace(0, 1, int(1/t) + 1)
+        heat.forward_diff(D, t, x, ts, xs)
+
+
+def part1b(h, k):
+    D = 2
+    for t, x in zip(k, h):
+        xs = np.linspace(0, 1, int(1/x) + 1)
+        ts = np.linspace(0, 1, int(1/t) + 1)
+        print(xs, ts)
+
+def part1c(h, k):
+    D = 2
+    for t, x in zip(k, h):
+        xs = np.linspace(0, 1, int(1/x) + 1)
+        ts = np.linspace(0, 1, int(1/t) + 1)
+        print(xs, ts)
+
+def part2():
+    pass
+
+def part3b():
+    pass
+
+def part3c():
     pass
 
 
